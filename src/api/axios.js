92a096c1a8b08ac814d5ab32c8f6284b604f7116
om-tripathi-1 <om.tripathi@axeno.co>
2026-09-10 08:48:54 +0530

    url fix

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://assignment-12-backend-v256.onrender.com/",
+  baseURL: "https://assignment-12-backend-v256.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
